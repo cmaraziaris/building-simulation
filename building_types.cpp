7a8657c762595b1e34bf718ad422781482a5168c
cMrzrs <sdi1800105@di.uoa.gr>
2019-10-24 00:25:48 +0300

    upl

--- building_types.cpp
+++ building_types.cpp
@@ -5,43 +5,29 @@ class visitor
 {
   int floor;
   int office_num;
-  unsigned int priority; 
+  int priority; 
 public:
   visitor();
   ~visitor();
-  void set_priority(unsigned int pr){  
+  void set_priority(int pr){  
     priority = pr;
   } // probz mporei na graftei kai ksexwrista gia readability
-  unsigned int get_priority(){
+  int get_priority(){
     return priority;
   }
 };
 
 /* ============================= */
-class building  //TODO: ADD STUFF
-{
-  unsigned int cap;  /* capacity */
-  unsigned int curr; /*  current ppl inside */
-  ground_level grd;
-  floor fl[4];
-public:
-  building();
-  ~building();
-  void enter(visitor v);
-  void exit(visitor v); // orismata?
-};
-
-/* ============================= */
 class waiting_room
 {
-  unsigned int cap;
-  unsigned int curr;
+  int cap;
+  int curr;
   visitor* visitors;
 public:
   waiting_room();
   ~waiting_room();
-  void enter(visitor v);
-  void exit(visitor v); // orismata?
+  void enter(visitor);
+  void exit(visitor); // orismata?
 };
 
 /* ============================= */
@@ -51,51 +37,73 @@ class ground_level
 public:
   ground_level();
   ~ground_level();
-  void enter(visitor v);
-  void exit(visitor v); // orismata?
-  void wait(visitor v);
+  void enter(visitor);
+  void exit(visitor); // orismata?
+  void wait(visitor);
 };
 
 /* ============================= */
 class office
 {
   int number;
-  unsigned int cap;
+  int cap;
   visitor* visitors;
 public:
   office();
   ~office();
-  void enter(visitor v);
-  void exit(visitor v);
+  void enter(visitor);
+  void exit(visitor);
 };
 
 /* ============================= */
 class floor
 {
-  unsigned int cap; //opt ? 
-  unsigned int curr;//opt ?
+  int cap; //opt ? 
+  int curr;//opt ?
   waiting_room wr;
   office off[10];
 public:
   floor();
   ~floor();
-  void enter(visitor v);
-  void exit(visitor v);
+  void enter(visitor);
+  void exit(visitor);
 };
 
 /* ============================= */
 class elevator
 {
-  unsigned int cap;
-  unsigned int curr;
+  int cap;
+  int curr;
   visitor* visitors;
 public:
   elevator();
   ~elevator();
   void operate();
-  void enter(visitor v);
-  void exit(visitor v);
-  void stop_up();  //TODO: orismata
+  void enter(visitor);
+  void exit(visitor);
+  void stop_up(); //TODO: orismata
   void stop_down(); //TODO: orismata  
   void empty_all(); //TODO: orismata  
 };
+
+
+/* ============================= */
+class building
+{
+  int cap;  /* capacity */
+  int curr; /*  current ppl inside */
+  ground_level grd;
+  floor fl[4];
+public:
+  building();
+  ~building();
+  void enter(visitor);
+  void exit(visitor); // orismata?
+};
+
+/* ============================= */
+
+int main(int argc, char const *argv[])
+{
+  return 0;
+}
