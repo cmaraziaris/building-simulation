39f32f874527cdaa291c81cb4769856b91a074c9
spChalk <sdi1800209>
2019-10-25 00:32:42 +0300

    Fixed list_remove to return the node that needs to exit

--- list_impl.cpp
+++ list_impl.cpp
@@ -14,19 +14,18 @@ visitor_list* list_insert (visitor* v,visitor_list* l) {
      } return n;
 }
 
-void list_remove (visitor* v,visitor_list** l) {
+visitor_list* list_remove (visitor* v,visitor_list** l) {    // Return the node that needs to be deleted
     visitor_list* tmp=*l,*prev;
 
     if ((tmp)&&(tmp->member->priority==v->priority)) {
         *l=tmp->link;
-        delete tmp;
-        return;
+        return tmp;
     }
     while ((tmp)&&(tmp->member->priority!=v->priority)) {
         prev=tmp;
         tmp=tmp->link;
     }
-    if (!tmp) return;
+    if (!tmp) return NULL;      // If there's no such node, return NULL
     prev->link=tmp->link;
-    delete tmp;
+    return tmp;
 }
